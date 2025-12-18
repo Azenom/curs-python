7a42f2883f911428f9e5315053f5a5546972c8b3
Paul Abesei <abeseipaul@gmail.com>
2025-12-18 19:08:18 +0200

    up

--- base-knowledge.py
+++ base-knowledge.py
@@ -151,6 +151,20 @@
 # print(nume[2]) # ------> litera u
 # nume = "abracadabra"
 # print(len(nume)) # --------> 11 caractere
+
+# var = 'Marius are pere'
+# print(var[0]) # ------> M
+# print(var[1]) # ------> a
+# print(var[-1]) # ------> e
+# print(var[-2]) # ------> r
+# print(var[-3]) # ------> e
+# print(var[-4]) # ------> p
+
+# print (var[-4::1]) # -------> pere
+# print (var[0:5:1]) # -------> Marius
+
+# var1 = var[7:10] # or var1 = var[7:10:1]
+# print(var1) # -------> are
 # ------------------------------------------------
 
 # liste
