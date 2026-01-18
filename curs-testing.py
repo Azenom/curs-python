37e0d37b5dea09eeba2b906f7fc438897ad3f74b
Paul Abesei <abeseipaul@gmail.com>
2026-01-18 13:14:40 +0200

    Update curs-testing.py

--- curs-testing.py
+++
@@ -1,7 +0,0 @@
-# 8) Elimină toate elementele pare dintr-o listă de numere.
-# Exemplu: [1,2,3,4,5,6] -> [1,3,5]
-lista = [1,2,3,4,5,6]
-for i in lista :
-    if i % 2 == 0 :
-        lista.remove(i)
-print(lista)
