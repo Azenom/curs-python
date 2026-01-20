7b681a4184ba3a26350c0d95033428f76e886a99
Paul Abesei <abeseipaul@gmail.com>
2026-01-20 13:51:56 +0200

    update

--- exercitii.py
+++ exercitii.py
@@ -1,7 +1,3 @@
-# Exerciții de vacanță – recapitulare Python (variabile, operatori, stringuri, control flow)
-# Perioada: 23 decembrie – 11 ianuarie
-
-# Încălzire (1-10):
 # 1. Creează două variabile cu valori numerice și afișează suma lor.
 
 # a = 5
@@ -575,3 +571,137 @@
 # print("Datele in ordine alfabetica dupa numele de familie:")
 # for date in date_intrare :
 #     print(date)
+
+# 55. Sa se scrie un program care sa implementeze un joc de tip quiz cu adevarat/fals. Sa se foloseasca o lista de intrebari.
+# La final sa se afiseze scorul obtinut de utilizator. Se poate folosii random pentru a amesteca intrebarile.
+
+# import random
+# name = input("Care este numele tau ? ")
+# print("Multumesc," + " " + name)
+# print("Regulile sunt urmatoarele :")
+# print("1. Set de intrebari cu punctaj si raspuns cu T sau F")
+# print("2. La final se afiseaza scorul")
+# scor = 0
+
+# varianta 1 -------------
+# lista = ["Suntem pe Terra?", "Suntem in Romania?", "Stai in apartament?", "Ai laptop?", "Ai tricou alb"]
+# random.shuffle(lista)
+# print("3. O sa avem " + str(len(lista)) + " intrebari")
+# for i in lista :
+#     print(i)
+#     if input() == "t" :
+#         print("Raspuns corect")
+#         scor = scor + 1
+#     else :
+#         print("Raspuns gresit")
+# -------------
+
+# sau
+
+# varianta 2 -------------
+# lista = [] # ------> aici este initializata o lista goala la care se adauga cu .append() elemente in aceasta
+# lista.append(["Suntem pe Terra?","t"])
+# lista.append(["suntem in Italia", "f"])
+# lista.append(["Stai la curte", "f"])
+# lista.append(["Ai laptop", "t"])
+# lista.append(["Ai tricou alb", "t"])
+# print("3. Vei avea " + str(len(lista)) + " intrebari.")
+# for i in lista :
+#     print(i[0])
+#     raspuns = input()
+#     if raspuns.lower() == i[1] : #------------> evitam ca utilizatorul sa introduca litera mare chiar daca e raspunsul corect
+#         print("Raspuns corect")
+#         scor = scor + 1
+#     else :
+#         print("Raspuns gresit")
+# ---------------
+
+# print("Scorul tau este : " + str(scor))
+#        #sau 
+# if scor == 0 :
+#     print("scorul tau este 0 , mai incearca")
+# elif scor <=4 :
+#     print("scorul tau este " + str(scor) + " , felicitari")
+# else :
+#     print("Felicitari ai atins scorul maxim de 5 puncte")
+
+# 56. Sa se sccrie un prog care verifica complexitatea unei parole. 
+# Cel putin 10 caractere, cel putin o litera, cel putin un caracter special, cel putin o cifra.
+
+# # create list for special characters
+# pool_special = [ '!' , '#' , '$' , '%' , '&' , '(' , ')' , '*' , '+' , ',' , '-' , '.' , '/' , ':' , ';' , '<' , '=' , '>' , '?' , '@' , '^' , '_' , '' , '|' , '~' ]
+
+# # create list for letters
+# pool_letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+
+# # create list for digits
+# pool_digits = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
+
+# # ask user to enter the password following the mentioned rules
+# print("The password's lenght must be grater than 10 and contain at least one letter, one special character and one digit")
+# password = input('Type your password : ')
+
+# # the key varriable turns true if the password meet the requirements
+# # start the loop as if the password doesn't meet the requirements
+# key = False
+# flag_special = False
+# flag_letters = False
+# flag_digits = False
+# flag_len = False
+
+# while key == False :    
+#     for p in password :
+#         for s in pool_special :
+#             if s == p :
+#                 flag_special = True
+#                 break
+#         for l in pool_letters :
+#             if l == p or l.upper() == p :
+#                 flag_letters = True
+#                 break
+#         for d in pool_digits :
+#             if d == p :
+#                 flag_digits = True
+#                 break
+#     if len(password) > 9 :
+#         flag_len = True
+#     if flag_special and flag_letters and flag_digits and flag_len :        
+#         key = True
+#     else :
+#         # print status to check and reset all flags in order to avoid partial checks
+#         print ('Special :', flag_special, 
+#                '---- Letter :',flag_letters, 
+#                '---- Digits :', flag_digits, 
+#                '---- Length :', flag_len)
+#         flag_special = False
+#         flag_letters = False
+#         flag_digits = False
+#         flag_len = False
+#         print('Requirements not met ')
+#         password = input('Re-Type your password : ')
+
+# if key == True :
+#     print('Your password is : ' + password)
+# else :
+#     print('Errrrr.....')
+
+# sau optimised version
+
+# def password_check(password) :
+#     if len(password) < 9 :
+#         print ('Not long enough')
+#         return False
+    
+#     big = any(c.isupper() for c in password)
+#     small = any(c.islower() for c in password)
+#     digit = any(c.isdigit() for c in password)
+#     alfanum = any(not c.isalnum() for c in password)
+
+#     return all([big, small, digit, alfanum])
+
+# password = input ("Enter password : ")
+
+# if password_check(password) :
+#     print("All good ✅ : ", password)
+# else:
+#     print("Not good enough ❌")
